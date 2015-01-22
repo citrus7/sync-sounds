afd21fc12c1eb066aadf623b0bf5b51c4e583bb9
junl <junlcn@gmail.com>
2015-01-22 01:49:15 -0500

    It works good on desktop browser; very slow on iOS

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('sound_key', function(msg){
+    io.emit('sound_key', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on port 3000');
+});
