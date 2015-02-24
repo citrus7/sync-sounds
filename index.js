acd3583af06acaea48ba02b154d5032c18c57b29
junl <junlcn@gmail.com>
2015-02-23 19:48:34 -0500

    Add jade template for admin page

--- index.js
+++ index.js
@@ -5,11 +5,30 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/admin', function(req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!'});
+  //res.sendFile(__dirname + '/index.html');
+  //var clients = io.sockets.adapter.rooms['room'];
+  //for (var clientId in clients) {
+  //  console.log(io.sockets.connected[clientId].id);
+  //}
+});
+
+app.get('/clients', function(req, res) {
+  var clients = io.sockets.adapter.rooms['room'];
+  res.json(clients);
+});
+
+
+
+
+
 io.on('connection', function(socket){
+  socket.join('room');
   socket.on('sound_key', function(msg){
     io.emit('sound_key', msg);
     //console.log('>> Receive sound ' + msg);
